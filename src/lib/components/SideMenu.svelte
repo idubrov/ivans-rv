32247f13fe6ef40a8ffa796084aea3c0a7b57a4a
Luba Dubrova <lyubov.dubrova@gmail.com>
2022-05-06 10:48:18 -0500

    🧭 Follow me

--- src/lib/components/SideMenu.svelte
+++ src/lib/components/SideMenu.svelte
@@ -15,6 +15,22 @@
 	{#if dev}
 		<section role="search">Search &#x1F50D;</section>
 	{/if}
+	<section id="follow">
+		<a target="_blank" href="https://t.me/s/ivans_rv"
+			><img
+				src="/src/lib/styles/icons8-telegram-24.png"
+				alt="Telegram icon"
+				style="object-fit: cover; width: 24px; height: 24px;"
+			/></a
+		>
+		<a target="_blank" href="https://t.me/s/ivans_rv"
+			><img
+				src="/src/lib/styles/icons8-instagram-24.png"
+				alt="Instagram icon"
+				style="object-fit: cover; width: 24px; height: 24px;"
+			/></a
+		>
+	</section>
 	<ul>
 		<li>
 			<span>Most recent</span>
